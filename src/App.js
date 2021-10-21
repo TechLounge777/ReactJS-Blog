62ac230ecbe073901ee35bf467028fef06a911de
TechLounge777 <msblanche777@gmail.com>
2021-10-21 21:00:59 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./index.css";
+import Navbar from "./Navbar";
+import Homepage from "./Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Homepage />
+      </div>
+    </div>
+  );
+}
+
+export default App;
