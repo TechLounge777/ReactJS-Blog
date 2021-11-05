d1fdf0a4b7034ddad2af775f6ec073b951431a2e
TechLounge777 <msblanche777@gmail.com>
2021-11-05 18:08:14 +0100

    Added conditional loading message

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,15 +5,23 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [posts, setPosts] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => res.json())
-      .then((data) => setPosts(data));
+      .then((res) => {
+        return res.json();
+      })
+
+      .then((data) => {
+        setPosts(data);
+        setIsLoading(false);
+      });
   }, []);
 
   return (
     <div className="home">
+      {isLoading && <div>Loading ...</div>}
       {posts && <BlogList posts={posts} user="Tom Smith" title="All Posts" />}
     </div>
   );
