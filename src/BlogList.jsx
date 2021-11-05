b4b59ed61aed7eec57f74c9846a402ad3c6fd7b8
TechLounge777 <msblanche777@gmail.com>
2021-11-05 17:52:00 +0100

    used useEffect and fetch method to fetch and display data

--- src/BlogList.jsx
+++ src/BlogList.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
-const BlogList = ({ blogs }) => {
+const BlogList = ({ posts, title, user }) => {
+  console.log(posts.user);
+
   return (
     <div className="blog-list">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
+      <h2>{title}</h2>
+      {posts.map((post) => (
+        <div className="blog-preview" key={post.id}>
+          <h2>{post.title}</h2>
+          <p>Written by {user}</p>
         </div>
       ))}
+      <button>Click me</button>
     </div>
   );
 };
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,27 +1,20 @@
 import React from "react";
 import { useState } from "react";
+import { useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]);
+  const [posts, setPosts] = useState(null);
 
-  const handleClick = () => {
-    setBlogs("Blogs");
-  };
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => setPosts(data));
+  }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} />
-      <button onClick={handleClick}>Click me</button>
+      {posts && <BlogList posts={posts} user="Tom Smith" title="All Posts" />}
     </div>
   );
 };
