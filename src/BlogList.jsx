f98c3af09fd68876ae0fc4e98fcd5156d9f83e0a
TechLounge777 <msblanche777@gmail.com>
2021-11-08 13:57:27 +0100

    added styles to blog preview list

--- src/BlogList.jsx
+++ src/BlogList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const BlogList = ({ posts, title, user }) => {
   return (
@@ -6,8 +6,10 @@ const BlogList = ({ posts, title, user }) => {
       <h2>{title}</h2>
       {posts.slice(0, 2).map((post) => (
         <div className="blog-preview" key={post.id}>
-          <h2>{post.title}</h2>
-          <p>Written by {user}</p>
+          <Link to={`/blogs/${post.id}`}>
+            <h2>{post.title}</h2>
+            <p>Written by {user}</p>
+          </Link>
         </div>
       ))}
       <button>Click me</button>
