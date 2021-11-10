73763b2997938022e7b7def23902b76a4804f73b
TechLounge777 <msblanche777@gmail.com>
2021-11-10 22:26:36 +0100

    added a form with controlled inputs

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { useState } from "react";
 
 const Create = () => {
-  return <div>Add a new blog</div>;
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("yoshi");
+  return (
+    <div className="create">
+      Add a new blog
+      <form>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
+    </div>
+  );
 };
 
 export default Create;
